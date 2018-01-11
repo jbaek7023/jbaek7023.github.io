6377e1bee318804f85526d2765fb4cd023361fa3
jbaek7023 <jbaek7023@gmail.com>
2018-01-11 00:02:27 -0500

    removed other files

--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import projects from './reducer_projects';
-import main_projects from './main_projects';
-const rootReducer = combineReducers({
-  projects: projects,
-  main_projects: main_projects
-});
-
-export default rootReducer;
