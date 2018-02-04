43cca2ff726b8ab198d6704eb158795916df80e1
jbaek7023 <jbaek7023@gmail.com>
2018-02-04 12:38:52 -0500

    adding project entry data

--- js/external.js
+++ js/external.js
@@ -30,14 +30,194 @@ var projectArray = [
     "time": 201700,
     "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
   }, {
-    "title": "YEAP Swap",
-    "description": "ASDASDASDeorgia Tech Student",
-    "depthDescription": "Facial keypoints include points around the eyes, nose, and mouth on any face and are used in many applications, from facial tracking to emotion recognition. Your completed code should be able to take in any image containing faces and identify the location of each face and their facial keypoints, as shown below.",
+    "title": "GT BookSwap",
+    "description": "an ASP.NET web application for GT students to exchange their textbooks",
+    "depthDescription": "Collaborated with four members to build an ASP.NET web application for GT students to exchange their textbooks.Articulated design through standard engineering diagrams (UML and Design Architecture Patterns) and conducted Product ,Backlog, Sprint Planning, Sprint Review & Sprint Retrospective meetings.",
     "tags": ["ad"],
     "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
     "stacks": ["Java", "C++"],
+    "contributor": ["John Baek (Me), JY Kim, Bongsu Kim, Guyoung Chung, SP Yoo"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Household app(Mint Clone)",
+    "description": "Android app to manage finances easier",
+    "depthDescription": "Household app is an android app to manage finaces easier.",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Java", "XML"],
+    "contributor": ["John Baek (Me), Guyoung Chung"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "IDCD",
+    "description": "IDCD allows industry, faculty, and students to submit project ideas for Capston Design. Students can then form groups and bid on which project to tackle during the course",
+    "depthDescription": "IDCD allows industry, faculty, and students to submit project ideas for Capston Design. Students can then form groups and bid on which project to tackle during the course. ",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Rails", "Ruby", "cucumber"],
+    "contributor": ["John Baek (Me) and other 33 classmates"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "CS 2110",
+    "description": "how computers work and how to program in C. ",
+    "depthDescription": "Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["sa"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Logism", "C", "Assembly"],
     "contributor": ["John Baek (Me)"],
-    "time": 201700,
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "CS 2200",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "Organization of the processor, Memory hierarchy, Storage devices, Parallel processors, Networking hardware, Software abstractions in the operating systems for orchestrating their usage, and Networking protocols to connect the computer system to its environment",
+    "tags": ["sa"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Logism", "C", "Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "CS 4400 - HOLD",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Website Optimization",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "jbaek7023.github.io",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Linux Web Configuration(Deployment)",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Catalog App",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Simple Blog",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Networking",
+    "description": "Provide a broad exposure to computer system structure and networking including software abstractions in operating systems for orchestrating the usage of the computing resources",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["Mongo DB"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201400,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "xv6 Implementation",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["sa"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "My First Socket",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["in"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "CS 4261-Mobile apps",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ad"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Automatic Cargo Design",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ai"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Sign Language Recognizer",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ai"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Apple Stock Recognizer & Sherlock Holmes",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ai"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
+    "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
+  }, {
+    "title": "Facial Keypoint Recognizer",
+    "description": "in progress",
+    "depthDescription": "1. Bits/Data Types/Operations, Digital Logic Structures, The Von Neumann Model, Introduction to simple microprocessor(LC-3), Assembly Language, I/O, TRAP Routines and Sub Routine, Stacks, C Programming",
+    "tags": ["ai"],
+    "image": "http://lorempixel.com/output/people-q-c-600-200-1.jpg",
+    "stacks": ["C, Assembly"],
+    "contributor": ["John Baek (Me)"],
+    "time": 201800,
     "srcCode" : 'https://github.com/jbaek7023/LC2200Processor-1'
   }
 ]
@@ -74,7 +254,7 @@ projects = {
               tagText = ", "
             }
             if(tag=="ai") {
-              tagText += "AI"
+              tagText += "Artificial Intelligence"
             } else if(tag=="ad") {
               tagText += "App Development"
             } else if(tag=="sa") {
